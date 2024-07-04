0514d337358df06ab6dde61a0e5cfda5511dbf9c
Yana Ivanova <yanaivashka@yandex.ru>
2024-07-04 21:20:23 +0300

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,21 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "no-console": "warn",
+    "import/prefer-default-export": "off",
+    "react/jsx-no-useless-fragment": "warn",
+    "no-unused-expressions": "error",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
